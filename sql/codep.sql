ee549288bd2da03565f9800e1c4107298add471a
muse1616 <1922330886@qq.com>
2020-05-11 19:35:26 +0800

    first commit

---
+++ sql/codep.sql
@@ -0,0 +1,34 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 50159
+ Source Host           : localhost:3306
+ Source Schema         : codep
+
+ Target Server Type    : MySQL
+ Target Server Version : 50159
+ File Encoding         : 65001
+
+ Date: 11/05/2020 17:19:40
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `username`(`username`) USING BTREE,
+  UNIQUE INDEX `password`(`password`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
--- utils/email/email.go
+++ utils/email/email.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"fmt"
+	"github.com/garyburd/redigo/redis"
 	"math/rand"
 	"net/smtp"
 	"server/dao"
@@ -54,6 +55,21 @@ func SaveEmailVerification(email string, code int) (err error) {
 	_, err = conn.Do("expire", email, 1800)
 	if err != nil {
 		return err
+	}
+	return
+}
+
+//根据email读取验证码
+func ReadVerificationByEmail(email string) (code string, err error) {
+	conn := dao.Pool.Get()
+	//ping
+	err = dao.Pool.TestOnBorrow(conn, time.Now())
+	if err != nil {
+		return
+	}
+	code, err = redis.String(conn.Do("Get", email))
+	if err != nil {
+		return
 	}
 	return
 }
